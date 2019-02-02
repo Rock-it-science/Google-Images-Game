d6520da442e92037907f644bf74cf5b25b0a44b2
Rock-it-science <wilmc17@gmail.com>
2019-02-01 21:43:19 -0800

    Users can join room

--- userSubmit.php
+++ userSubmit.php
@@ -15,7 +15,17 @@ $roomCheck = $conn->query("SELECT 1 FROM " . $roomCode . ";");
 if($roomCheck){//Valid room code
   //Add username to corresponding table
   $conn->query("INSERT INTO " . $roomCode . " VALUES ('". $name ."', 0);");
-    echo 'joined game';
+  echo 'joined game';
+  //Check if only player in game
+  $rows = $conn->query("SELECT * FROM " . $roomCode . ";");
+  if(mysqli_num_rows($rows) == 1){
+    //Show button to start game
+    //TODO implement a system for starting the game from this button
+    ?>
+    <br>
+    <button onclick="">Start Game</button>
+    <?php
+  }
 }else{//invalid room code
   echo "invalid room code";
 }
